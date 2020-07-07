f2d1600cd60db2b75bbb9d1958494f961cad9360
John Browning <johnb@redhat.com>
2020-07-07 13:12:54 -0400

    update command

--- startnginx.sh
+++ startnginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 HOSTNAME=`hostname`
-EC2_INSTANCES=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==`Name`]|[0].Value}' --output table`
+EC2_INSTANCES=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==\`Name\`]|[0].Value}' --output table`
 echo -e "<h3>EC2 Instances</h3>\n<p>$EC2_INSTANCES </p></div></body>" >> index.html
 echo -e "<footer class='text-center'>\n<p>Served from container: $HOSTNAME running on $CLUSTERENV</p>\n</footer>\n</html>\n" >> index.html
 
