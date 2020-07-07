80e9da626cb7c69b97712fa2887f5422e64b921e
John Browning <johnb@redhat.com>
2020-07-07 12:15:12 -0400

    updated html

--- startnginx.sh
+++ startnginx.sh
@@ -2,7 +2,7 @@
 
 HOSTNAME=`hostname`
 EC2_INSTANCES=`aws ec2 describe-instances`
-echo -e "<h3>EC2 Instances</h3>\n<p>$EC2_INSTANCES </p>" >> index.html
+echo -e "<h3>EC2 Instances</h3>\n<p>$EC2_INSTANCES </p></div></body>" >> index.html
 echo -e "<footer class='text-center'>\n<p>Served from container: $HOSTNAME running on $CLUSTERENV</p>\n</footer>\n</html>\n" >> index.html
 
 nginx -g "daemon off;"
